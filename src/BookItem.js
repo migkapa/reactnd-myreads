cd17102a6f4ed5e75fddc2b6020465c947f2976b
Migen Karriqi <migkapa@migen.local>
2020-03-07 16:21:16 +0100

    remove some inline style

--- src/BookItem.js
+++ src/BookItem.js
@@ -12,8 +12,6 @@ const BookItem = props => {
         <div
           className="book-cover"
           style={{
-            width: 128,
-            height: 193,
             backgroundImage: `url(${thumbnail})`
           }}
         ></div>
